2719675a27b1bb3683279dfb6e40b63d90482eba
Aislynn Uribe <aislynn.rose@gmail.com>
2021-07-18 21:51:08 -0600

    updated path names in apiRoutes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,27 +1,27 @@
 
 const router = require('express').Router();
 const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+const { v1: uuidv1 } = require('uuid');
 
-const store = require('../db/store.js');
+const db = require('../db/db.json');
 
 router.get('/notes', (req, res) => {
-  let database = JSON.parse(fs.readFileSync('../db/store.js'));
+  let database = JSON.parse(fs.readFileSync('../db/db.json'));
   res.JSON(database);
 });
 
 router.post('/notes', (req, res) => {
-  let database = JSON.parse(fs.readFileSync('../db/'));
+  let database = JSON.parse(fs.readFileSync('../db/db.json'));
   const initNote = {
-    ...req.body, id: uuidv4(),
+    ...req.body, id: uuidv1(),
   };
   database.push(initNote);
-  fs.writeFileSync('../db/store.js', JSON.stringify(database));
+  fs.writeFileSync('../db/db.json', JSON.stringify(database));
   res.json(database);
 });
 
 router.delete('/notes/:id', (req, res) => {
-  let database =JSON.parse(fs.readFileSync('../db/store.js'));
+  let database =JSON.parse(fs.readFileSync('../db/db.json'));
 });
 
 module.exports = router;
