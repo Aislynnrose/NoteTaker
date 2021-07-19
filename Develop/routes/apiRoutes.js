b8f9c1870f3959050e9cc631bc7cfcae268352a6
Aislynn Uribe <aislynn.rose@gmail.com>
2021-07-18 21:22:12 -0600

    updated apiRoutes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,11 +1,27 @@
 
 const router = require('express').Router();
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 const store = require('../db/store.js');
 
-// router.get, notes, post route, delete route and
-
-router.get('/', (req, res) => {
-  store
-  .read
+router.get('/notes', (req, res) => {
+  let database = JSON.parse(fs.readFileSync('../db/store.js'));
+  res.JSON(database);
 });
+
+router.post('/notes', (req, res) => {
+  let database = JSON.parse(fs.readFileSync('../db/'));
+  const initNote = {
+    ...req.body, id: uuidv4(),
+  };
+  database.push(initNote);
+  fs.writeFileSync('../db/store.js', JSON.stringify(database));
+  res.json(database);
+});
+
+router.delete('/notes/:id', (req, res) => {
+  let database =JSON.parse(fs.readFileSync('../db/store.js'));
+});
+
+module.exports = router;
